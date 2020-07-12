4cd9f48b25c96c4f3c5748a732d20f8dccf18139
Jhonnyalx123 <jhonnyzapat@gmail.com>
2020-07-11 22:05:53 -0500

    Logout

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,15 +1,13 @@
 import { Injectable } from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
-import { promise } from 'protractor';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
+import {Router} from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private AFauth: AngularFireAuth) { }
+  constructor(private AFauth: AngularFireAuth, private router:Router) { }
 
   login(email:string, password:string){
     return new Promise((resolve,rejects)=>{
@@ -18,5 +16,8 @@ export class AuthService {
       }).catch(err=>rejects(err));
     });
     
+  }
+  OnLogout(){
+    this.AFauth.signOut().then(res=>this.router.navigate([""])).catch(err=>console.log("Error: "+err));
   }
 }
