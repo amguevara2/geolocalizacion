c272331188414d50dc693f68153113276bb249f4
Alexis <amguevara2@espe.edu.ec>
2020-07-11 23:32:56 -0500

    MapaV3

--- src/app/mapa/mapa.page.ts
+++ src/app/mapa/mapa.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController, NumericValueAccessor } from '@ionic/angular';
 
 declare var google;
 
@@ -9,26 +10,49 @@ declare var google;
   styleUrls: ['./mapa.page.scss'],
 })
 export class MapaPage implements OnInit {
-
-  constructor(private geolocation: Geolocation) { }
-
+  mapRef = null;
+  constructor(private geolocation: Geolocation, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.loadMap();
   }
 
   async loadMap(){
-   const rta= await this.geolocation.getCurrentPosition();
-   const myLatLng = {
-    lat: rta.coords.latitude,
-    lng: rta.coords.longitude
-  };
-  console.log(myLatLng);
-  const mapEle: HTMLElement = document.getElementById('map');
-  const map = new google.maps.Map(mapEle, {
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+    const myLatLng= await this.ObtenerUbicacion();
+    const mapEle: HTMLElement = document.getElementById('map');
+    this.mapRef = new google.maps.Map(mapEle, {
     center: myLatLng,
     zoom: 12
   }); 
+  google.maps.event
+.addListenerOnce(this.mapRef, 'idle', () => {
+  // loaded
+  loading.dismiss();
+  this.Addmarcador(myLatLng.lat, myLatLng.lng);
+  
+});
   }
+
+private Addmarcador(lat: number, lng: number){
+  const marker = new google.maps.Marker({
+    position: {lat,lng},
+    zoom: 8,
+    map: this.mapRef  ,
+    title: 'Hello World!'
+  });
+}
+
+private async ObtenerUbicacion(){
+  const rta= await this.geolocation.getCurrentPosition();
+  return {
+   lat: rta.coords.latitude,
+   lng: rta.coords.longitude
+ };
+
+}
+
+
 
 }
